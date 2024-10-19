25a180e41ef47a00abd17a9820b584345e0626fd
wiut00013374 <Hp@LAPTOP-1N1SQG72>
2024-10-19 22:19:04 +0500

    Your commit message

--- CompanyMicroserviceAPI/Program.cs
+++ CompanyMicroserviceAPI/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IProductService, ProductService>();
 
 // Add services to the container.
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(
@@ -21,6 +20,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure Kestrel to listen on all network interfaces
+app.Urls.Add("http://*:5000");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
