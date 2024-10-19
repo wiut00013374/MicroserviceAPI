a808280a739b56ceb5fefc1a6ca7eba176a52140
wiut00013374 <Hp@LAPTOP-1N1SQG72>
2024-10-19 22:40:58 +0500

    fifth commit

---
+++ CompanyMicroserviceAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyMicroserviceAPI.Controllers
+{
+    public class HealthCheckController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
